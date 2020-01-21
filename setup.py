db4413fffc5b8b54ddab510f999009c9accbba9a
Zack Ankner <zackankner@gmail.com>
2020-01-21 10:46:57 -0500

    feat(master): can now intialize project as current dir

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
   name='tf2-initializer',
-  version='0.1',
+  version='0.2',
   scripts=['tf2-initializer'],
   author='Zack Ankner',
   author_email='zackankner@gmail.com',
