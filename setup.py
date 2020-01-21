75f3c819a996bc3a9d81798675ae9dcc5af49e7b
Zack Ankner <zackankner@gmail.com>
2020-01-21 09:47:09 -0500

    fix(master): propper required imports

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+  long_description = f.read()
+
+
+setuptools.setup(
+  name='tf2-initializer',
+  version='0.1',
+  scripts=['tf2-initializer'],
+  author='Zack Ankner',
+  author_email='zackankner@gmail.com',
+  description='A tf2 project initializer',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  install_requires=[
+    'importlib_resources'
+  ],
+  url='https://github.com/zankner/tf2-initializer',
+  packages=setuptools.find_packages(),
+  classifiers=[
+    'Programming Language :: Python :: 3',
+    'Operating System :: Unix'
+  ]
+)
